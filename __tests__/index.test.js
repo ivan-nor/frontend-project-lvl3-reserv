27587ee8f9f4de5ad3f0dc7fbf7898713bbea1aa
ivan-nor <ivan.norik@gmail.com>
2020-04-02 17:43:37 +0300

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 // import jumbotron from '../src/app/jumbotron';
 // import path from 'path';
-import app from '../src/app';
+import app from '../src/app/application';
 
 const expected = 'app/index.js is runned';
 
